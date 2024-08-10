49088db28ecb0692f897cfecdeb7c4b3804aac16
CSRuggeri <santiruu@gmail.com>
2024-08-09 22:55:55 -0300

    cors moddleware

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { db } from './src/database/db.js';
 import router from './src/routes/mainRouter.js';
-
+import cors from 'cors'
 const app = express();
 const port = 3000;
 
+app.use(cors("*"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
